4d5e350a7734ef2955d260d2ea8f75f9e08c8a5b
seanwayland <seanwayland@gmail.com>
2018-10-14 14:07:46 -0400

    Add files via upload

---
+++ PitchShift.h
@@ -0,0 +1,16 @@
+//
+//  PitchShift.h
+//
+//
+//  Created by sean wayland on 10/12/18.
+//
+
+#ifndef PitchShift_h
+#define PitchShift_h
+
+void smbFft(float *fftBuffer, long fftFrameSize, long sign);
+double smbAtan2(double x, double y);
+void smbPitchShift(float pitchShift, long numSampsToProcess, long fftFrameSize, long osamp, float sampleRate, float *indata, float *outdata);
+
+#endif /* PitchShift_h */
+
